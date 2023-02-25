bbeb5a19683f7151de34baacde2a0c737fbee13b
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-02-25 20:09:38 +0800

    Add "Pangrams" Python solution

---
+++ pangrams/solution.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    used_chars = set()
+    lowered_s = s.lower()
+    for char in lowered_s:
+        used_chars.add(char)
+    if len(used_chars) == 27:
+        return "pangram"
+    return "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
