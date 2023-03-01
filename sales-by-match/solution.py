874234f606892bd63f0694075df620833a6939ac
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-03-01 16:45:36 +0800

    Add "Sales by Match" Python solution

---
+++ sales-by-match/solution.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    socks = set()
+    pair_count = 0
+    for idx in range(n):
+        if ar[idx] in socks:
+            pair_count += 1
+            socks.remove(ar[idx])
+            continue
+        socks.add(ar[idx])
+    return pair_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
