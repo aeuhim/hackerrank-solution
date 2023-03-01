4d39e43575b577f8bd377b6278c0282407b5fef2
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-03-01 15:51:44 +0800

    Add "Day 3: Arrays" JavaScript solution

---
+++ 10-days-of-javascript/day-3-arrays/solution.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let first = Number.NEGATIVE_INFINITY
+    let second = Number.NEGATIVE_INFINITY
+    for (let num of nums) {
+        if (first == num) {
+            continue
+        }
+        if (first < num) {
+            second = first
+            first = num
+            continue
+        }
+        if (second < num) {
+            second = num
+        }
+    }
+    return second
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
