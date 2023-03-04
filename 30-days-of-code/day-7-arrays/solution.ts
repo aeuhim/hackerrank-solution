89708f5407ab87d3a68ca864fb88fa544e670230
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-03-04 10:59:27 +0800

    Add "Day 7: Arrays" TypeScript solution

---
+++ 30-days-of-code/day-7-arrays/solution.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+
+
+function main() {
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    let result: string = ""
+    for (let idx = arr.length; idx > 0;) {
+        result += arr[--idx] + " "
+    }
+    console.log(result)
+}
