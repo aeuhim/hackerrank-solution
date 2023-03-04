d020be1f5ff8c836159dda41ae09e6dc68e0b9a2
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-03-04 12:53:23 +0800

    Add "Maximum Perimeter Triangle" Python solution

---
+++ maximum-perimeter-triangle/solution.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximumPerimeterTriangle' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY sticks as parameter.
+#
+
+def maximumPerimeterTriangle(sticks):
+    # Write your code here
+    sticks = sorted(sticks, reverse=True)
+    for idx in range(len(sticks) - 2):
+        if sticks[idx] < sticks[idx+1] + sticks[idx+2]:
+            return [sticks[idx+2], sticks[idx+1], sticks[idx]]
+    return [-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    sticks = list(map(int, input().rstrip().split()))
+
+    result = maximumPerimeterTriangle(sticks)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
