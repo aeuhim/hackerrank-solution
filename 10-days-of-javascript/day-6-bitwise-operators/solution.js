0c74d908a4560b08986fcdaa7b9ad4137b51c817
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-03-08 12:18:52 +0800

    Add "Day 6: Bitwise Operators" JavaScript solution

---
+++ 10-days-of-javascript/day-6-bitwise-operators/solution.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getMaxLessThanK(n, k) {
+    let result = 0
+    for (let a = 1; a <= n; a++) {
+        for (let b = a + 1; b <= n; b++) {
+            let currentResult = a & b
+            if (result < currentResult && currentResult < k) {
+                result = currentResult
+            }
+        }
+    }
+    return result
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
