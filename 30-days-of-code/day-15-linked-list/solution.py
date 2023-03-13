01dfe7f0c46ca4736b33bf15f4218c6adc930750
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-03-13 20:48:38 +0800

    Add "Day 15: Linked List" Python solution

---
+++ 30-days-of-code/day-15-linked-list/solution.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        node = head
+        if head == None:
+            return Node(data)
+        while(node.next != None):
+            node = node.next
+        node.next = Node(data)
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
